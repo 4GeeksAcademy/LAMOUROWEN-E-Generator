668974aff6d39dd3800ac35f949fc9902415ec41
LAMOUROWEN <140284905+LAMOUROWEN@users.noreply.github.com>
2023-09-16 03:24:01 +0000

    Final stage

--- src/app.js
+++ src/app.js
@@ -6,6 +6,43 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse(); //write your code here
+  });
 };
+
+let generateExcuse = () => {
+  let pronoun = ["A", "My"];
+  let subject = ["dog", "grandma", "turtle", "my bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let possession = ["my homework", "the keys", "the car"];
+  let when = [
+    "this morning",
+    "last night",
+    "this evening",
+    "around 8 O'Clock on Tuesday",
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let pronounIndex = [Math.floor(Math.random() * pronoun.length)];
+  let subjectIndex = [Math.floor(Math.random() * subject.length)];
+  let actionIndex = [Math.floor(Math.random() * action.length)];
+  let possessionIndex = [Math.floor(Math.random() * possession.length)];
+  let whenIndex = [Math.floor(Math.random() * when.length)];
+
+  return (
+    pronoun[pronounIndex] +
+    " " +
+    subject[subjectIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    possession[possessionIndex] +
+    " " +
+    when[actionIndex]
+  );
+};
+console.log();
